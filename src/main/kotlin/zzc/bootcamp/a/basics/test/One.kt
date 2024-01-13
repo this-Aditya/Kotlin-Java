80bf6d6890c5d061597051cd2be0e2a38fffeaba
this-Aditya <adityamishra2217@gmail.com>
2024-01-13 14:24:04 +0530

    test file added

--- src/main/kotlin/zzc/bootcamp/a/basics/test/One.kt
+++ src/main/kotlin/zzc/bootcamp/a/basics/test/One.kt
@@ -1,8 +1,12 @@
 package zzc.bootcamp.a.basics.test
 
+import okhttp3.internal.immutableListOf
+
 fun main() {
-    val chars = listOf('a','b','c','d','e','f','g')
-    for (i in chars.indices) {
-        println(chars[i])
-    }
+//    val chars = listOf('a','b','c','d','e','f','g')
+//    for (i in chars.indices) {
+//        println(chars[i])
+//    }
+
+    val maps = immutableListOf<String>()
 }
